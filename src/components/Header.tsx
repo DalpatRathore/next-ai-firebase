68cfaa8ebe4cf2e4edb846ecb4d8909e45cc3600
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-08-08 17:28:16 +0530

    feat: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,46 @@
+import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import {
+  FaArrowRightFromBracket,
+  FaArrowRightToBracket,
+} from "react-icons/fa6";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex items-center justify-between shadow-sm p-5">
+      <Link href={"/"}>
+        <Image src={"/logo.svg"} width={50} height={50} alt="logo"></Image>
+      </Link>
+      {!true ? (
+        <div className="">
+          <Button size={"lg"} className="hidden md:flex">
+            <FaArrowRightToBracket className="mr-2" />
+            Login
+          </Button>
+          <Button size={"icon"} className="flex md:hidden" title="login">
+            <FaArrowRightToBracket />
+          </Button>
+        </div>
+      ) : (
+        <div className="">
+          <Button size={"lg"} className="hidden md:flex" variant={"outline"}>
+            <FaArrowRightFromBracket className="mr-2" />
+            Logout
+          </Button>
+          <Button
+            size={"icon"}
+            className="flex md:hidden"
+            title="logout"
+            variant={"outline"}
+          >
+            <FaArrowRightFromBracket />
+          </Button>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default Header;
